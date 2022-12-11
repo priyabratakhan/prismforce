3d20ffb141d882623e00fb7ceea986882e00c8bf
Priyabrata Khan <93465026+priyabratakhan@users.noreply.github.com>
2022-12-11 10:36:15 +0530

    test2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const express = require("express");
+
+const userController = require("./controllers/user.controller");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", userController);
+
+module.exports = app;
